e3d0eac345437ec01bf060affbef07d791aa2b53
Rodrigo Bosisio <rod.bosisio@gmail.com>
2020-03-25 18:21:02 -0300

    Updates

---
+++ precache-manifest.8a09bbbeb94763de8dfcf2fa87ecf993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40efa6a0b3946f404000f80d81e2fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfefb610590f8a3b30db",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b743ae55a3ea077d30b",
+    "url": "/static/js/2.d8d8e335.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8d8e335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfefb610590f8a3b30db",
+    "url": "/static/js/main.d5bc6235.chunk.js"
+  },
+  {
+    "revision": "8fe3a7fe8a0a23f9adc5",
+    "url": "/static/js/runtime-main.08e8f87d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
