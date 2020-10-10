55627550bd4a8133a80f6a0b1ce28b27b33e2e4c
Rodrigo Bosisio <rod.bosisio@gmail.com>
2020-10-10 17:32:01 -0300

    add skills progress bars

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <ProfileHeader :picture-url="require('../assets/profile-pic.jpg')" name="Rodrigo Bosisio" />
-    <div class="social-links">
+    <div class="home__social-links">
       <IconLink
         :blank="true"
         label="LinkedIn"
@@ -16,19 +16,31 @@
       />
       <IconLink label="Contato" fa-icon="far fa-envelope" href="#" />
     </div>
+    <div class="home__skills">
+      <h2>Skills</h2>
+      <div class="home__skills__progress">
+        <ProgressBar label="Back-end" progress="90" />
+        <ProgressBar label="Front-end" progress="60" />
+        <ProgressBar label="Cloud" progress="75" />
+        <ProgressBar label="Agile" progress="85" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import ProfileHeader from '@/components/ProfileHeader.vue'; // @ is an alias to /src
-import IconLink from '@/components/IconLink.vue'; // @ is an alias to /src
+
+import IconLink from '@/components/IconLink.vue';
+import ProfileHeader from '@/components/ProfileHeader.vue';
+import ProgressBar from '@/components/ProgressBar.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: {
     ProfileHeader,
     IconLink,
+    ProgressBar,
   },
 });
 </script>
@@ -38,12 +50,34 @@ export default Vue.extend({
   display: flex;
   flex-direction: column;
 
-  .social-links {
+  &__social-links {
     display: flex;
     justify-content: center;
 
     & > * {
-      margin-right: 15px;
+      margin-right: 16px;
+    }
+  }
+
+  &__skills {
+    margin-top: 36px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & > h2 {
+      margin-bottom: 24px;
+    }
+
+    &__progress {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+
+      & > * {
+        margin-bottom: 16px;
+      }
     }
   }
 }
